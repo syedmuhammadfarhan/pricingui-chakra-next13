cb7a72201542e60dbafef4ccbaf370b357c00648
syedmuhammadfarhan <farhanpiaic@gmail.com>
2023-02-21 14:34:46 +0500

    after redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
-// import Image from "next/image";
-// import { Inter } from "@next/font/google";
-// import styles from "./page.module.css";
 
-// const inter = Inter({ subsets: ["latin"] });
-import { ChakraProvider, Heading, Text } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import Features from "./Features";
 import Header from "./Header";
-import Practice from "./Practice";
 import Pricing from "./Pricing";
 
 export default function Home() {
